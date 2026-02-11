b39485bc5894e9c6358d7d2855b10bc1fe10ac56
Julieta Castillo <julietatcastillo04@gmail.com>
2026-02-11 00:23:38 -0300

    conexion de formulario con el estado

--- Repaso/src/assets/components/MiFormulario.jsx
+++ Repaso/src/assets/components/MiFormulario.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const MiFormulario = () => {
+    //capturando datos del formulario
+    const [formData, setFormData] = useState({
+        username: "",
+        email: "ejemplo@ejemplo.com",
+        password: ""
+    })
+
     //metodo para capturar los datos
     const activarPorEvento = (evento) => {
         console.log('evento activado', evento.type);
@@ -21,16 +28,16 @@ const MiFormulario = () => {
             <h2>Gestion de eventos y formularios</h2>
             <form autoComplete='off' onClick={formularioActivado}>
                 <div>
-                    <label className="form-label" htmlFor="username" onMouseOver={activarPorEvento}>Usuario: </label>
-                    <input className="form-control" type="text" id='username' name='username' onClick={inputActivado}/>
+                    <label className="form-label" htmlFor="username">Usuario: </label>
+                    <input className="form-control" type="text" id='username' name='username' value={formData.username}/>
                 </div>
                 <div>
                     <label className="form-label" htmlFor="email">Email: </label>
-                    <input className="form-control" type="text" id='email' name='email' onChange={activarPorEvento}/>
+                    <input className="form-control" type="text" id='email' name='email' value={formData.email}/>
                 </div>
                 <div>
-                    <label className="form-label" htmlFor="password" onClick={activarPorEvento} >Contraseña: </label>
-                    <input className="form-control" type="password" id='password' name='password' />
+                    <label className="form-label" htmlFor="password">Contraseña: </label>
+                    <input className="form-control" type="password" id='password' name='password' value={formData.password}/>
                 </div>
                 <br />
                 <button type='submit'>Enviar!</button>
