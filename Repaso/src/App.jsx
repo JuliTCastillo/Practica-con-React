5523ec235cf4ed88b4d423c22d5d04943e056aa2
Julieta Castillo <julietatcastillo04@gmail.com>
2026-02-11 14:59:37 -0300

    Renderizado de lista

--- Repaso/src/App.jsx
+++ Repaso/src/App.jsx
@@ -1,14 +1,13 @@
 import './App.css'
 import ConstruyendoComponente from './assets/components/ConstruyendoComponente'
 import MiFormulario from './assets/components/MiFormulario'
+import Catalogo from './assets/components/Catalogo'
 
 function App() {
 
   return (
     <>
-      <ConstruyendoComponente/>
-      <hr/>
-      <MiFormulario></MiFormulario>
+      <Catalogo></Catalogo>
     </>
   )
 } 
---
+++ Repaso/src/assets/components/Catalogo.jsx
@@ -0,0 +1,25 @@
+const Catalogo = () => {
+    const productos = [
+        { id: 1, nombre: "Camiseta", precio: 20.99 },
+        { id: 2, nombre: "Pantalon", precio: 35.50 },
+        { id: 3, nombre: "Zapatos", precio: 60.0 },
+        { id: 4, nombre: "Bolso", precio: 25.75 },
+        { id: 5, nombre: "Gorra", precio: 15.0 },
+        { id: 6, nombre: "Medias", precio: 5.0 },
+    ]
+
+    return(
+        <>
+            <h2>Catalogo de productos</h2> 
+            <ul>
+                {productos.map(
+                    (prod) => (
+                        <li>{prod.nombre} - {prod.precio}</li>
+                    )
+                )}
+            </ul>
+        </>
+    )
+}
+
+export default Catalogo;
