df7a9521ac930c7d62ceec8944e346f81b5d4c88
Julieta Castillo <julietatcastillo04@gmail.com>
2026-02-23 18:24:15 -0300

    uso de styled components

--- Repaso/src/assets/components/Libro/Libro.jsx
+++ Repaso/src/assets/components/Libro/Libro.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import './libro.css'
+import styled from 'styled-components'; //importamos la libreria
 
 export const Libro = ({libro}) => {
+
+    const Titulo = styled.h2`
+        display:block;
+        color: red;
+    `
 
     return (
         <>
             <div className='item'>
-                <h2>{libro.titulo}</h2>
+                <Titulo>{libro.titulo}</Titulo>
                 <span>Publicado en {libro.publicado}</span>
             </div>
         </>
