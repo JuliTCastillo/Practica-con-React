d7af09244052e819d4be5b981883010a38bc87ed
Julieta Castillo <julietatcastillo04@gmail.com>
2026-02-23 18:41:10 -0300

    interaccion con css

--- Repaso/src/assets/components/Libro/Libro.jsx
+++ Repaso/src/assets/components/Libro/Libro.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './libro.css'
 import styled from 'styled-components'; //importamos la libreria
 
@@ -8,13 +8,16 @@ export const Libro = ({libro}) => {
         display:block;
         color: red;
     `
+    const [miClase, setMiClase] = useState('default');
+    
 
     return (
         <>
-            <div className='card col-md-3'>
+            <div className={'card col-md-3 ' + miClase}>
                 <div className='card-body'>
                     <h5 className='card-title'>{libro.titulo}</h5>
                     <p className='card-text'>Publicado en {libro.publicado}</p>
+                    <button className='btn btn-primary' onClick={()=>{setMiClase('seleccionado')}}>Seleccionar</button>
                 </div>
             </div>
         </>
